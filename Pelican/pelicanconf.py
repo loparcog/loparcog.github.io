292ef6acf44143c6581819dea2398dc71bcd437e
Giacomo Loparco <gdloparco@gmail.com>
2022-11-13 14:43:15 -0500

    Added Papyrus theme

--- Pelican/pelicanconf.py
+++ Pelican/pelicanconf.py
@@ -1,31 +1,62 @@
 AUTHOR = 'Giacomo Loparco'
 SITENAME = 'Giacomo'
-SITEURL = ''
-
-PATH = 'content'
-
+SITEURL = 'https://loparcog.github.io'
 TIMEZONE = 'America/Toronto'
-
 DEFAULT_LANG = 'en'
 
+SUBTITLE = 'Loparco'
+SUBTEXT = 'software engineer exploring tech they find interesting, main focus on audio programming'
+COPYRIGHT = '©2022'
+PATH = 'content'
+THEME = 'themes/Papyrus'
+THEME_STATIC_PATHS = ['static']
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['readtime', 'search', 'neighbors', 'pelican-toc']
+
+DISPLAY_PAGES_ON_MENU = True
+DIRECT_TEMPLATES = (('index', 'search', 'tags', 'categories', 'archives',))
+PAGINATED_TEMPLATES = {'index':None,'tag':None,'category':None,'author':None,'archives':24,}
+
+# Site search plugin
+SEARCH_MODE = "output"
+SEARCH_HTML_SELECTOR = "main"
+# Table of Content Plugin
+TOC = {
+    'TOC_HEADERS'       : '^h[1-3]', # What headers should be included in
+                                     # the generated toc
+                                     # Expected format is a regular expression
+    'TOC_RUN'           : 'true',    # Default value for toc generation,
+                                     # if it does not evaluate
+                                     # to 'true' no toc will be generated
+    'TOC_INCLUDE_TITLE': 'false',    # If 'true' include title in toc
+}
+
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+RSS_FEED_SUMMARY_ONLY = True
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# Social widgets
+SOCIAL = (
+    ('github', 'https://github.com/loparcog'),
+    ('twitter', 'https://twitter.com/gxacomo'),
+)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Article share widgets
+SHARE = (
+    ("twitter", "https://twitter.com/intent/tweet/?text=Features&amp;url="),
+    ("linkedin", "https://www.linkedin.com/sharing/share-offsite/?url="),
+    ("reddit", "https://reddit.com/submit?url="),
+    ("telegram", "https://telegram.me/share/url?text=Features&amp;url="),
+)
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 8
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# DISQUS_SITENAME = ''
+# GOOGLE_ANALYTICS = ''
--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Right now I'm trying [Pelican](https://getpelican.com/).
 ## How are you deploying
 
 The entire project is in the Pelican directory. Entering this directory and running
+
 `pelican content -o .. -s pelicanconf.py`
+
 will put the output to the main directory where GitHub will fetch and host it.
